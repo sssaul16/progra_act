476f38f836e46e16eac5fcb7ed7fa406194f6dd7
sssaul16 <alanismorriset10@gmail.com>
2025-08-26 13:22:39 -0600

    commit pruba.py

---
+++ pruba.py
@@ -0,0 +1,6 @@
+nom_1 = "Ariana"
+nom_2 = "ariana"
+
+print(nom_1 == nom_2)
+#print(nom_1 = nom_2 )
+# pues marca error porque es como si estuvieras renombrando una variable porque el signo de igual se usa para eso solamente 
