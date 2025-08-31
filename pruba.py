4667ac53b78b62b333ee751fb95d8c0fea60f54c
Alan Saul <167271309+sssaul16@users.noreply.github.com>
2025-08-30 19:27:30 -0600

    Delete pruba.py

--- pruba.py
+++
@@ -1,6 +0,0 @@
-nom_1 = "Ariana"
-nom_2 = "ariana"
-
-print(nom_1 == nom_2)
-#print(nom_1 = nom_2 )
-# pues marca error porque es como si estuvieras renombrando una variable porque el signo de igual se usa para eso solamente 
